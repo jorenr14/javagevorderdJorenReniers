0d0342ae063ccb6894208ede318ff87d4de0146a
JorenR <reniers.joren@gmail.com>
2024-07-04 14:29:30 +0200

    Vierde commit

---
+++ JavaGevorderdExamen/src/main/java/com/javaGevorderdExamen/resource/AddToDoToUserDto.java
@@ -0,0 +1,52 @@
+package com.javaGevorderdExamen.resource;
+
+import java.time.LocalDate;
+
+public class AddToDoToUserDto {
+
+    private String titel;
+    private String commentaar;
+    private boolean status;
+    private LocalDate expiryDate;
+    private int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getCommentaar() {
+        return commentaar;
+    }
+
+    public void setCommentaar(String commentaar) {
+        this.commentaar = commentaar;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
